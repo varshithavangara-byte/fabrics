73edf485cdc7b1bf900efac9e6bd4ad19146b9b3
Varshitha <varshithavangara@gmail.com>
2026-07-27 08:06:24 +0530

    dharmavaram saree for gallery 2

--- client/src/data/gallery.js
+++ client/src/data/gallery.js
@@ -107,7 +107,7 @@ export const galleryImages = [
   },
   {
     id: 14,
-    src: './images/gallery/dharmavaram-silk.jfif',
+    src: './images/gallery/dharmavaram-saree.jfif',
     alt: 'Dharmavaram silk saree',
     category: 'silk-sarees',
     title: 'Dharmavaram Silk',
